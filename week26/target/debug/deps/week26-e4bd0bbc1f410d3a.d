c02e9b45c1924a92c68d8e4b909124fd3549da9b
SuryaS1729 <143543633+SuryaS1729@users.noreply.github.com>
2025-08-07 18:38:30 +0530

    Ignore target folders and Cargo.lock files

--- week26/target/debug/deps/week26-e4bd0bbc1f410d3a.d
+++
@@ -1,5 +0,0 @@
-/Users/saaketsurya/Desktop/solana/rust/rust-learning/kirat bootcamp/RustingUp/week26/target/debug/deps/week26-e4bd0bbc1f410d3a.d: src/main.rs
-
-/Users/saaketsurya/Desktop/solana/rust/rust-learning/kirat bootcamp/RustingUp/week26/target/debug/deps/libweek26-e4bd0bbc1f410d3a.rmeta: src/main.rs
-
-src/main.rs:
