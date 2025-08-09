9abe1b035db30343891afbbeeb9f66923761cf41
SuryaS1729 <143543633+SuryaS1729@users.noreply.github.com>
2025-08-09 18:26:27 +0530

    inited rust part 2

--- week26/src/main.rs
+++ week26/src/main.rs
@@ -1,3 +1,30 @@
+// fn main() {
+//     let oddity = is_even(32);
+
+//     println!("{}", oddity)
+// }
+//numbers in rust
+// fn sum(a: u32, b: u32) -> u32 {
+//     return a + b;
+// }
+
+// //booleans in rust
+
+// fn is_even(a: u32) -> bool {
+//     return a % 2 == 0;
+// }
+// strings in rust
+
+// fn main() {
+//     let name: String = String::from("sahithi");
+
+//     println!("{}", name)
+// }
+
+//vectors in rust
+
 fn main() {
-    println!("Hello, world!");
+    let v: Vec<i32> = vec![1, 2, 3];
+
+    println!("{:?}", v)
 }
