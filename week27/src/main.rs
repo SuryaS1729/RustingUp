058846f77c367cf537c832f4c191b1de70366f47
SuryaS1729 <143543633+SuryaS1729@users.noreply.github.com>
2025-08-10 11:39:48 +0530

    mut and immutable referencing

--- week27/src/main.rs
+++ week27/src/main.rs
@@ -1,3 +1,10 @@
+//borrowing and ownership in rust
+
 fn main() {
-    println!("Hello, world!");
+    let mut s1: String = String::from("sahithi");
+    // s1.push_str("loves surya"); //-> here you cannot mutate because, variables are immutable by default
+    let s2: &mut String = &mut s1;
+    s2.push_str(" loves surya");
+
+    println!("{}", s2)
 }
