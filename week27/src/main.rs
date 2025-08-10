fc2715e8de7e78d862ef3f92366200564553107b
SuryaS1729 <143543633+SuryaS1729@users.noreply.github.com>
2025-08-10 15:26:32 +0530

    structs in rust

--- week27/src/main.rs
+++ week27/src/main.rs
@@ -1,14 +1,37 @@
 //borrowing and ownership in rust
 
+// fn main() {
+//     let mut s1: String = String::from("sahithi");
+//     let s2 = &mut s1;
+//     s2.push_str(" likes surya");
+//     let s3 = &s1;
+// }
+
+//structs in rust
+//
+//
+
+struct Rect {
+    height: f32,
+    width: f32,
+}
+
+impl Rect {
+    fn area(&self) -> f32 {
+        return self.width * self.height;
+    }
+
+    fn print_something(a: u32) {
+        println!("{}, {}", "static function", a) // more like singletons or object class in kotlin and javascript
+    }
+}
 fn main() {
-    let mut s1: String = String::from("sahithi");
-    // s1.push_str("loves surya"); //-> here you cannot mutate because, variables are immutable by default
-    let s2: &mut String = &mut s1;
-    //once you have a mutable reference, you cannot have other references, immutable or mutable to the variable.
-    let s3: &mut String = &mut s1;
-    s2.push_str(" loves surya");
+    let r = Rect {
+        width: 10.0,
+        height: 12.0,
+    };
 
-    s3.push_str("  won't marry surya"); // -> yay, thank god rust does'nt allow this to compile
-
-    println!("{}", s2)
+    println!("{}, {}", r.width, r.height);
+    println!("{}", r.area());
+    Rect::print_something(10);
 }
