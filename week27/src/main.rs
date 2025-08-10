332098900393cc81e0ba14a32bca8c71ae5e7454
SuryaS1729 <143543633+SuryaS1729@users.noreply.github.com>
2025-08-10 11:56:55 +0530

    how many mutable references allowed?

--- week27/src/main.rs
+++ week27/src/main.rs
@@ -4,7 +4,11 @@ fn main() {
     let mut s1: String = String::from("sahithi");
     // s1.push_str("loves surya"); //-> here you cannot mutate because, variables are immutable by default
     let s2: &mut String = &mut s1;
+    //once you have a mutable reference, you cannot have other references, immutable or mutable to the variable.
+    let s3: &mut String = &mut s1;
     s2.push_str(" loves surya");
+
+    s3.push_str("  won't marry surya"); // -> yay, thank god rust does'nt allow this to compile
 
     println!("{}", s2)
 }
