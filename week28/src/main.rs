8eec77c19c9fe1da42021c9ad89c1e7824acf7de
SuryaS1729 <143543633+SuryaS1729@users.noreply.github.com>
2025-08-13 18:52:39 +0530

    traits and traitbounds

--- week28/src/main.rs
+++ week28/src/main.rs
@@ -39,24 +39,75 @@
 // }// see how we added the display trait here to the generic type to let rust know that this type has this trait
 // //so rust allows us to run those operations
 
-//Generics over Structs
-#[derive(Clone, Copy)]
-struct Rect<T> {
-    width: T,
-    height: T,
+// //Generics over Structs
+// #[derive(Clone, Copy)]
+// struct Rect<T> {
+//     width: T,
+//     height: T,
+// }
+
+// impl<T: std::ops::Mul<Output = T> + Copy> Rect<T> {
+//     fn area(&self) -> T {
+//         return self.width * self.height;
+//     }
+// }
+// fn main() {
+//     let r1 = Rect {
+//         width: 10.0,
+//         height: 102.0,
+//     };
+
+//     let area = r1.area();
+//     println!("{}", area)
+// }
+
+//generics over enums
+// enum Option<T>{
+// Some(T)
+// None
+// }
+
+//traits
+// traits are similar to interfaces in ts
+
+use std::f32::consts::PI;
+
+//implement a trait over a struct
+//
+trait Shape {
+    fn area(&self) -> f32;
+}
+struct Rect {
+    width: f32,
+    height: f32,
+}
+struct Circle {
+    radius: f32,
 }
 
-impl<T: std::ops::Mul<Output = T> + Copy> Rect<T> {
-    fn area(&self) -> T {
+impl Shape for Rect {
+    fn area(&self) -> f32 {
         return self.width * self.height;
     }
 }
-fn main() {
-    let r1 = Rect {
-        width: 10.0,
-        height: 102.0,
-    };
 
-    let area = r1.area();
-    println!("{}", area)
+impl Shape for Circle {
+    fn area(&self) -> f32 {
+        return PI * self.radius * self.radius;
+    }
+}
+
+fn print_area_of_shape<T: Shape>(s: T) {
+    //the generic is bound to the trait Shape
+    println!("{}", s.area())
+}
+fn main() {
+    let r = Rect {
+        width: 20.0,
+        height: 21.0,
+    };
+    let c = Circle { radius: 2.0 };
+
+    print_area_of_shape(c);
+    print_area_of_shape(r);
 }
