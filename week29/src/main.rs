f67a786e3659ca49f4e878fc2afb8505131e1d50
SuryaS1729 <143543633+SuryaS1729@users.noreply.github.com>
2025-08-14 14:26:55 +0530

    copy, clone ownership macros

--- week29/src/main.rs
+++ week29/src/main.rs
@@ -27,21 +27,21 @@
 // procedural macros are used to write custom derive traits, attribute-like macros, and function-like macros
 //
 //1. Custom derive Macros
-#[derive(Debug)]
-struct User {
-    name: String,
-    age: u32,
-}
+// #[derive(Debug)]
+// struct User {
+//     name: String,
+//     age: u32,
+// }
 
-fn main() {
-    let u = User {
-        name: String::from("Sahithi"),
-        age: 23,
-    };
+// fn main() {
+//     let u = User {
+//         name: String::from("Sahithi"),
+//         age: 23,
+//     };
 
-    // print!("{}", u); // this will not work as User does not implement the Display trait
-    print!("{:?}", u); // this will work as Userdoes not implement the Debug trait, will work when we add #[derive(Debug)]
-}
+//     // print!("{}", u); // this will not work as User does not implement the Display trait
+//     print!("{:?}", u); // this will work as Userdoes not implement the Debug trait, will work when we add #[derive(Debug)]
+// }
 
 // 2. Attribute-like Macros
 
@@ -53,3 +53,51 @@ fn main() {
 //annotations work at run time, macros work at compile time
 
 //implementing macros, how to use them, instead of the internals
+//debug macro exists, display macro does not exist, we can implement it ourselves
+// use std::fmt::Display;
+
+// #[derive(Debug)]
+// struct User {
+//     name: String,
+//     age: u32,
+// }
+
+// impl Display for User {
+//     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+//         write!(f, "User: {}, Age: {}", self.name, self.age)
+//     }
+// }
+// fn main() {
+//     let u = User {
+//         name: String::from("Sahithi"),
+//         age: 23,
+//     };
+
+//     print!("{}", u); // this will not work as User does not implement the Display trait
+//     print!("{:?}", u); // this will work as Userdoes not implement the Debug trait, will work when we add #[derive(Debug)]
+// }
+
+//copy clone macros
+//
+// fn main() {
+//     let user1 = String::from("Sahithi");
+//     let user2 = user1;
+
+//     print!("{},{}", user1, user2)//ownership rulesrevision
+// }
+//
+#[derive(Debug, Clone, Copy)]
+struct User {
+    is_male: bool,
+    age: u32,
+}
+fn main() {
+    let u1 = User {
+        is_male: true,
+        age: 23,
+    };
+
+    let u2 = u1; // this will not work as User does not implement the Copy trait, we can implement the Copy trait by adding #[derive(Copy, Clone)] to the struct
+
+    print!("{:?},{:?}", u1, u2)
+}
