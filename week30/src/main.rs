9f3a26f1d78f5fc0b13bd17523f38c33d52c53d6
SuryaS1729 <143543633+SuryaS1729@users.noreply.github.com>
2025-10-21 15:56:33 +0530

    week30 bregins

---
+++ week30/src/main.rs
@@ -0,0 +1,78 @@
+// The following code is an example of how to use the serde library for serialization and deserialization.
+// It is currently commented out.
+
+// // Import the `Deserialize` and `Serialize` traits from the `serde` library.
+// use ::serde::{Deserialize, Serialize};
+
+// // The `#[derive]` attribute automatically implements the specified traits for the struct.
+// // - `Serialize`: Allows the struct to be converted into a serialized format (e.g., JSON).
+// // - `Deserialize`: Allows the struct to be created from a serialized format.
+// // - `Debug`: Allows the struct to be printed for debugging purposes using `{:?}`.
+// #[derive(Serialize, Deserialize, Debug)]
+// struct User {
+//     name: String,
+//     age: u8,
+// }
+
+// // The main function for the serde example.
+// fn main() {
+//     // Create an instance of the `User` struct.
+//     let user = User {
+//         name: String::from("Alice"),
+//         age: 30,
+//     };
+
+//     // Serialize the `user` instance into a JSON string.
+//     // `serde_json::to_string` returns a `Result` which will be `Ok(json_string)` on success
+//     // or `Err(error)` on failure.
+//     let serialized = serde_json::to_string(&user);
+//     match serialized {
+//         Ok(json) => println!("Serialized: {}", json),
+//         Err(e) => eprintln!("Serialization error: {}", e),
+//     }
+
+//     // The following line for deserialization is incorrect and would cause a compile error.
+//     // It should take a string slice as an argument.
+//     // For example:
+//     // let json_data = r#"{"name":"Bob","age":25}"#;
+//     // let deserialized: User = serde_json::from_str(json_data).unwrap();
+//     // println!("Deserialized: {:?}", deserialized);
+//     let deserialized = serde_json::from_str::serialized;
+// }
+
+// This is the current main function that will be executed.
+//
+//
+
+use borsh::{BorshDeserialize, BorshSerialize};
+
+#[derive(BorshSerialize, BorshDeserialize, Debug, Clone)]
+
+struct User {
+    username: String,
+    password: String,
+}
+
+// fn main() {
+//     let u: User = User {
+//         username: String::from("Saaket"),
+//         password: String::from("sahithi"),
+//     };
+
+//     let mut v: Vec<u8> = Vec::new();
+
+//     let ans = u.serialize(&mut v);
+
+//     match ans {
+//         Ok(_) => print!("{:?}", v),
+//         Err(_) => print!("Error occurred during serialization"),
+//     }
+// }
+//
+//
+fn main() {
+    let u = User {
+        username: String::from("Saaket"),
+        password: String::from("sahithi"),
+    };
+}
